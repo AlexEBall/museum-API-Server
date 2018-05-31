1b806b9f93e6b9e1329a32304ee1eda00a08468d
Alex Edward Ball <ballalexedward@gmail.com>
2018-05-30 18:26:19 -0700

    building out frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-class App extends Component {
+export default class App extends Component {
   render() {
+    const store = createStore(reducers, applyMiddleware(ReduxThunk, logger));
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            {/* <Route exact path="/agents" component={AgentsPage}/> */}
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
-
-export default App;
