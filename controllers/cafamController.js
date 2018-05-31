889d21ed0bb135f9e514addc7172712772c13ea2
Alex Edward Ball <ballalexedward@gmail.com>
2018-05-30 18:21:28 -0700

    added crud functionality on the backend

--- controllers/cafamController.js
+++ controllers/cafamController.js
@@ -7,5 +7,30 @@ module.exports = {
             .sort({ _id: 1 })
             .then(cafamModel => res.json(cafamModel))
             .catch(err => res.status(422).json(err));
+    },
+    findByID: (req, res, next) => {
+        db.cafam
+            .findById(req.params.id)
+            .then(cafamModel => res.json(cafamModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.cafam
+            .create(req.body)
+            .then(cafamModel => res.json(cafamModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.cafam
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(cafamModel => res.json(cafamModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.cafam
+            .findById({ _id: req.params.id })
+            .then(cafamModel => cafamModel.remove())
+            .then(cafamModel => res.json(cafamModel))
+            .catch(err => res.status(422).json(err));
     }
 }
--- routes/api/cafam.js
+++ routes/api/cafam.js
@@ -2,16 +2,14 @@ const router = require("express").Router();
 const cafamController = require("../../controllers/cafamController");
 
 // Matches with "/api/cafam"
-router
-    .route("/")
-    .get(cafamController.findAll);
-    // .post(cafamController.create);
+router.route("/")
+    .get(cafamController.findAll)
+    // should be at the route they are posting from form
+    .post(cafamController.create);
 
-// Matches with "/api/books/:id"
-// router
-//     .route("/:id")
-//     .get(cafamController.findById)
-//     .put(cafamController.update)
-//     .delete(cafamController.remove);
+router.route("/:id")
+    .get(cafamController.findByID)
+    .put(cafamController.update)
+    .delete(cafamController.remove);
 
 module.exports = router;
