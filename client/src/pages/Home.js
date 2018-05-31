4c57572a68daf97c735cc9bca0b8f0a600603862
Alex Edward Ball <ballalexedward@gmail.com>
2018-05-30 20:56:28 -0700

    trying to get dev server to update with sass without building

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,8 +3,12 @@ import React, {Component} from 'react';
 export default class Home extends Component {
     render() {
         return(
-            <div>
-                <h1>Hello</h1>
+            <div className="wrapper">
+                <header>
+                    <nav>
+                        <h2 className="nav-title">CAFAM Mobile App CMS</h2>
+                    </nav>
+                </header>
             </div>
         );
     }
