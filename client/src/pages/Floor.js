33fd40d15dfef41166c54f0a301d00973c22f133
Alex Edward Ball <ballalexedward@gmail.com>
2018-06-23 11:58:03 -0700

    adding some styling to the exhibition floor pages

--- client/src/pages/Floor.js
+++ client/src/pages/Floor.js
@@ -2,24 +2,29 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchFloor} from '../actions';
 import API from "../utils/API";
+import Header from '../components/Header';
 // import {Link} from 'react-router-dom';
 
 class Floor extends Component {
-    componentWillMount() {
-        // API.getExhibitionFloor(this.props.match.params.floor)
-        // .then(res => {
-        //     console.log('the response', re)
-        //     this.props.fetchFloor({ floor: res.data[0].floors[0] })
-        // })
-        // .catch(err => console.log(err));
+    componentWillMount = () => {
         const floor = parseInt(this.props.match.params.floor);
         this.props.fetchFloor(floor);
+    }
+
+    renderFloorInformation = () => {
+        
     }
 
     render() {
         console.log('is this working?', this.props);
         return (
-            <h1>Jumpy</h1>
+            <div className="wrapper">
+                <Header />
+                <section className="exhibitions">
+                </section>
+                <footer className="footer">
+                </footer>
+            </div>
         );
     }
 };
