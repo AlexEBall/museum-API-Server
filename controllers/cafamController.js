b7927111cdf0ed7a0bac9af7a6876a3fc0e042c7
Alex Edward Ball <ballalexedward@gmail.com>
2018-06-28 17:37:02 -0700

    working on one will try others now

--- controllers/cafamController.js
+++ controllers/cafamController.js
@@ -57,14 +57,11 @@ module.exports = {
         console.log(req);
         const floor = parseInt(req.params.floor);
         const position = parseInt(req.body.position);
+        const mongoSetNestedArrayPositionFilterString = `floors.$[element].floorGallery.${position}`;
+        console.log('The string=====', mongoSetNestedArrayPositionFilterString);
         db.cafam
             .updateOne({ 'floors.floor': floor},
-            {$set: { 'floors.$[element].floorGallery': 
-                {
-                $each: [req.body.pictureInput],
-                $position: position
-                } 
-                }},
+            {$set: { [mongoSetNestedArrayPositionFilterString]: req.body.pictureInput }},
             { arrayFilters: [{ 'element.floor': floor }]})
             .then(cafamModel => res.json(cafamModel))
             .catch(err => res.status(422).json(err));
@@ -82,6 +79,7 @@ module.exports = {
 //             {$set: { 'floors.$[element].audioLink': 'testywoooooo' }},
 //             { arrayFilters: [{ 'element.floor': 1 }]});
 
+// this will push something in to my nested array but won't necissarily update one
 // db.getCollection('cafam').updateOne({ 'floors.floor': 1},
 //              {$push: { 'floors.$[element].floorGallery': 
 //                  {
@@ -90,3 +88,9 @@ module.exports = {
 //                  } 
 //                  }},
 //              { arrayFilters: [{ 'element.floor': 1 }]});
+
+// will update seems like but how to pass the position into the string?
+// works but have to wrap the whole concated sting in []
+// db.getCollection('cafam').updateOne({ 'floors.floor': 1},
+//             {$set: { 'floors.$[element].floorGallery.2': 'testywoooooo' }},
+//             { arrayFilters: [{ 'element.floor': 1 }]});
