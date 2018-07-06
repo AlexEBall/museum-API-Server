f1d71ce9058786c0e3f8ff41a478a0638128e189
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-05 20:42:36 -0700

    rendering program page with all info

--- client/src/components/ProgramCard.js
+++ client/src/components/ProgramCard.js
@@ -6,20 +6,25 @@ export default class ProgramCard extends Component {
         return(
             <div className="programCard">
                 <div className="programCard__Title">
-                    Hey
+                    {this.props.program.title}
                 </div>
                 <div className="programCard__Img">
-                    You
+                    <img className="programCard__Img--picture" src={this.props.program.picture} alt="program picture" />
                 </div>
                 <div className="programCard__Price">
+                    {this.props.program.price}
                 </div>
                 <div className="programCard__Description">
+                    {this.props.program.description}
                 </div>
                 <div className="programCard__Time">
+                    {this.props.program.time}
                 </div>
                 <div className="programCard__MemberInfo">
+                    {this.props.program.memberInfo}
                 </div>
                 <div className="programCard__RegistrationLink">
+                    {this.props.program.registrationLink}
                 </div>
             </div>
         )
