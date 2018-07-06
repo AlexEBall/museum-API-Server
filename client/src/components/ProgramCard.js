e626d8e7a19d44b42d65613887d084344cdd1312
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-06 15:28:07 -0700

    program page buttons added

--- client/src/components/ProgramCard.js
+++ client/src/components/ProgramCard.js
@@ -5,26 +5,40 @@ export default class ProgramCard extends Component {
         console.log('these are my props', this.props);
         return(
             <div className="programCard">
-                <div className="programCard__Title">
+                <div className="programCard programCard__Title">
                     {this.props.program.title}
                 </div>
-                <div className="programCard__Img">
+                <div className="programCard programCard__Img">
                     <img className="programCard__Img--picture" src={this.props.program.picture} alt="program picture" />
                 </div>
-                <div className="programCard__Price">
-                    {this.props.program.price}
+                <div className="programCard programCard__Price">
+                    Price: {this.props.program.price}
                 </div>
-                <div className="programCard__Description">
-                    {this.props.program.description}
+                <div className="programCard programCard__Description">
+                    Description: {this.props.program.description}
                 </div>
-                <div className="programCard__Time">
-                    {this.props.program.time}
+                <div className="programCard programCard__Time">
+                    Time: {this.props.program.time}
                 </div>
-                <div className="programCard__MemberInfo">
-                    {this.props.program.memberInfo}
+                <div className="programCard programCard__MemberInfo">
+                    Member Information: {this.props.program.memberInfo}
                 </div>
-                <div className="programCard__RegistrationLink">
-                    {this.props.program.registrationLink}
+                <div className="programCard programCard__RegistrationLink">
+                    Registration Link: {this.props.program.registrationLink}
+                </div>
+                <div className="programCard programCard__Buttons">
+                    <button 
+                        className="exhibitions__btn"
+                        onClick={this.addProgram}>Add a Program
+                    </button>
+                    <button 
+                        className="exhibitions__btn"
+                        onClick={this.editProgram}>Edit a Program
+                    </button>
+                    <button 
+                        className="exhibitions__btn"
+                        onClick={this.deleteProgram}>Delete a Program
+                    </button>
                 </div>
             </div>
         )
