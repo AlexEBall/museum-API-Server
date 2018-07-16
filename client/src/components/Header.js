b7f12d27d82986140356bd7322572c1d6913e0fb
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-15 18:30:03 -0700

    finished styling for now, some minor stuff to come back to

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
         <header className="header">
             <nav className="nav">
-                <h2 className="nav__title">CAFAM Mobile App CMS</h2>
+                <Link to="/" className="nav__homePageLink">
+                    <h2 className="nav__title">The Craft and Folk Art Museum</h2>
+                </Link>
                 <div className="nav__space-box"></div>
                 <div className="nav__links-box">
                     <ul className="nav__links-ul">
